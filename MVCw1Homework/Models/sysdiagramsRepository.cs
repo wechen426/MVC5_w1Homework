d1ca7ce18b72c923f946c7e6b4cbc47e28d0d3b0
wechen_name <te426odin@gmail.com>
2017-03-13 16:49:21 +0800

    建立 repos 中繼資料處理 class

---
+++ MVCw1Homework/Models/sysdiagramsRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+	
+namespace MVCw1Homework.Models
+{   
+	public  class sysdiagramsRepository : EFRepository<sysdiagrams>, IsysdiagramsRepository
+	{
+
+	}
+
+	public  interface IsysdiagramsRepository : IRepository<sysdiagrams>
+	{
+
+	}
+}
