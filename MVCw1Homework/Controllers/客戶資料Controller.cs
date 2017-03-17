ef6e15ef8ea3ab469cb535625331e8a01afec30e
wechen_name <te426odin@gmail.com>
2017-03-17 16:54:53 +0800

    客戶資料-OK

---
+++ MVCw1Homework/Controllers/客戶資料Controller.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCw1Homework.Models;
+
+namespace MVCw1Homework.Controllers
+{
+    public class 客戶資料Controller : Controller
+    {
+        private 客戶資料Entities1 db = new 客戶資料Entities1();
+        客戶資料Repository repo = RepositoryHelper.Get客戶資料Repository();
+        // GET: 客戶資料
+        public ActionResult Index(string keyword)
+        {
+             var data = repo.All().AsQueryable();
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                data = data.Where(客戶資料 => 客戶資料.客戶名稱.Contains(keyword));
+            }
+            ViewBag.keyword = keyword;
+            return View(data.ToList());
+        }
+
+        // GET: 客戶資料/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶資料 客戶資料 = repo.Find(id.Value);
+            if (客戶資料 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶資料);
+        }
+
+        // GET: 客戶資料/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: 客戶資料/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,客戶名稱,統一編號,電話,傳真,地址,Email,是否刪除")] 客戶資料 客戶資料)
+        {
+            if (ModelState.IsValid)
+            {
+                repo.Add(客戶資料);
+                repo.UnitOfWork.Commit();
+                return RedirectToAction("Index");
+            }
+
+            return View(客戶資料);
+        }
+
+        // GET: 客戶資料/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶資料 客戶資料 = repo.Find(id.Value);
+            if (客戶資料 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶資料);
+        }
+
+        // POST: 客戶資料/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,客戶名稱,統一編號,電話,傳真,地址,Email,是否刪除")] 客戶資料 客戶資料)
+        {
+            if (ModelState.IsValid)
+            {
+                var db = repo.UnitOfWork.Context;
+                db.Entry(客戶資料).State = EntityState.Modified;
+                repo.UnitOfWork.Commit();
+                return RedirectToAction("Index");
+            }
+            return View(客戶資料);
+        }
+
+        // GET: 客戶資料/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶資料 客戶資料 = repo.Find(id.Value);
+            if (客戶資料 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶資料);
+        }
+
+        // POST: 客戶資料/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            客戶資料 客戶資料 = repo.Find(id);
+            repo.Delete(客戶資料);
+            repo.UnitOfWork.Commit();
+            return RedirectToAction("Index");
+        }
+
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        db.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
+    }
+}
